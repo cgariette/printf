a03540d8716a8922538c58c9a4e89814e125459c
cgariette <clifftonafande@gmail.com>
2023-11-14 01:33:01 -0800

    print numbers

--- main.h
+++ main.h
@@ -13,8 +13,58 @@
 #define F_HASH 8
 #define F_SPACE 16
 
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
 int get_width(const char *format, int *i, va_list list);
 int get_flags(const char *format, int *i);
 int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
+/**
+ * struct fmt - Structure
+ *
+ * @fmt: the format
+ * @fn: The function associated
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: format
+ * @fn: function associated
+ */
+typedef struct fmt fmt_t;
+
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+int _printf(const char *format, ...);
+
+
+/* print unsigned int */
+int print_unsigned(va_list types, char buffer[],
+                int flags, int width, int precision, int size);
+
+/* prints unsigned number in octal */
+int print_octal(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+
+/* prints unsigned number in hexadecimal */
+int print_hexadecimal(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+
+/* print unsigned number in upper hexadecimal */
+int print_hexa_upper(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+
+/* prints hex number in lower or upper */
+iint print_hexa(va_list types, char map_to[], char buffer[],
+        int flags, char flag_ch, int width, int precision, int size);
 
 #endif
