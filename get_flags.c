ff06dc63577deb52c95a95aa7cb3da172e1cb162
cgariette <clifftonafande@gmail.com>
2023-11-13 23:44:57 -0800

    get flags and get width functions

---
+++ get_flags.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * get_flags - calculates the active flags
+ * @format: formatted string to print the arguments
+ * @i: takes a parameter
+ *
+ * Return: flags
+ */
+int get_flags(const char *format, int *i)
+{
+	int j, current_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (current_i = *i + 1; format[current_i] != '\0'; current_i++)
+	{
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[current_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
+		if (FLAGS_CH[j] == 0)
+			break;
+	}
+
+	*i = current_i -1;
+
+	return (flags);
+}
--- main.h
+++ main.h
@@ -6,5 +6,14 @@
 
 #define BUFF_SIZE 1024
 
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+int get_width(const char *format, int *i, va_list list);
+int get_flags(const char *format, int *i);
 
 #endif
