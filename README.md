27887022d0d3d0dab11117a9537a34517b3d612e
cgariette <clifftonafande@gmail.com>
2023-11-13 23:00:23 -0800

    Printf function

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a collaboration project for Printf function with Cliff Afande and Karabo Mokherere
---
+++ _printf.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+void print_buffer(char buffer[], int *buffer_index);
+
+/**
+ * _printf - printf function
+ * @format: format specifier
+ *
+ * Return: printed characters
+ */
+
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buffer_index = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buffer_index] == format[i];
+			if(buffer_index == BUFFSIZE)
+				print_buffer(buffer, &buffer_index);
+			/*write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buffer_index);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
+			if printed == -1)
+				return (-1);
+			printed_chars = printed_chars + printed;
+		}
+	}
+
+	print_buffer(buffer, &buffer_index);
+
+	va_end(list);
+
+	return (printed_chars);
+
+}
+
+/**
+ * print_buffer - prints the contents of the buffer
+ * @buffer: an array of characters
+ * @buffer_index: Index at which to add the next character
+ */
+void print_buffer(char buffer[], int *buffer_index)
+{
+	if (*buffer_index > 0)
+		write(1, &buffer[0], *buffer_index);
+
+	*buffer_index = 0;
+}
